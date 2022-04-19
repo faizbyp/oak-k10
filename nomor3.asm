1c764f4c3bcb51681a6d3420437a52a5ffa791a5
Faiz Byputra <65883882+faizbyp@users.noreply.github.com>
2022-04-19 08:40:40 +0700

    Create nomor3.asm

---
+++ nomor3.asm
@@ -0,0 +1,20 @@
+section .data
+    d: dd 0         ; dd = define double-word (4 byte integer)
+    a: dd 1
+    b: dd 3
+    c: dd 2
+    
+section .text
+    MOV EAX, [b]    ; d = b*b - (4 * a * c)
+    IMUL [b]        ; Mengalikan register (b) dengan b
+    MOV [d], EAX    ; Menyimpan b^2 ke dalam memori d
+    
+    MOV EAX, 4      ; Mengganti nilai register EAX dengan 4
+    IMUL [a]        ; Mengalikan register dengan memori a
+    IMUL [c]        ; Mengalikan register dengan memori c
+    SUB [d], EAX    ; Mengurangi memori d dengan nilai register
+    MOV EAX, [d]    ; Memindahkan nilai memori d ke dalam register
+
+    INT 1           ; interrupt, mencetak register EAX
+    
+    HLT             ; Halt, mematikan CPU
